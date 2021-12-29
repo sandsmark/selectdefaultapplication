9b7e7621a42966c79b20fa8fb5ba5742af3b1dcf
magnus <magnus@iastate.edu>
2021-12-29 15:02:48 -0600

    Add vscode and clang-format based on linux kernel

--- widget.h
+++ widget.h
@@ -10,40 +10,40 @@ class QTreeWidget;
 class QListWidget;
 class QPushButton;
 
-class Widget : public QWidget
-{
-    Q_OBJECT
+class Widget : public QWidget {
+	Q_OBJECT
 
 public:
-    Widget(QWidget *parent = nullptr);
-    ~Widget();
-
+	Widget(QWidget *parent = nullptr);
+	~Widget();
 
 private slots:
-    void onMimetypeSelected();
-    void onSetDefaultClicked();
+	void onMimetypeSelected();
+	void onSetDefaultClicked();
 
 private:
-    void loadDesktopFile(const QFileInfo &fileInfo);
-    void setDefault(const QString &appName, const QSet<QString> &mimetypes, const QSet<QString> &unselectedMimetypes);
-    void loadIcons(const QString &path);
+	void loadDesktopFile(const QFileInfo &fileInfo);
+	void setDefault(const QString &appName, const QSet<QString> &mimetypes,
+			const QSet<QString> &unselectedMimetypes);
+	void loadIcons(const QString &path);
 
-    QMultiHash<QString, QString> m_supportedMimetypes;
-    QMultiHash<QString, QString> m_childMimeTypes;
-    QHash<QString, QSet<QString>> m_applications;
-    QHash<QString, QString> m_applicationIcons;
-    QHash<QString, QString> m_applicationNames;
-    QHash<QString, QString> m_desktopFileNames;
+	QMultiHash<QString, QString> m_supportedMimetypes;
+	QMultiHash<QString, QString> m_childMimeTypes;
+	QHash<QString, QSet<QString> > m_applications;
+	QHash<QString, QString> m_applicationIcons;
+	QHash<QString, QString> m_applicationNames;
+	QHash<QString, QString> m_desktopFileNames;
 
-    QHash<QString, QString> m_appIdToDesktopFile;
+	QHash<QString, QString> m_appIdToDesktopFile;
 
-    QHash<QString, QIcon> m_mimeTypeIcons; // for preloading icons, because that's (a bit) slooow
-    QHash<QString, QString> m_iconPaths;
+	QHash<QString, QIcon>
+		m_mimeTypeIcons; // for preloading icons, because that's (a bit) slooow
+	QHash<QString, QString> m_iconPaths;
 
-    QTreeWidget *m_applicationList;
-    QMimeDatabase m_mimeDb;
-    QListWidget *m_mimetypeList;
-    QPushButton *m_setDefaultButton;
+	QTreeWidget *m_applicationList;
+	QMimeDatabase m_mimeDb;
+	QListWidget *m_mimetypeList;
+	QPushButton *m_setDefaultButton;
 };
 
 #endif // WIDGET_H
