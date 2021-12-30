9e4b869edc7cbdc07fdf60d31be866a39053215a
magnus <magnus@iastate.edu>
2021-12-30 05:12:58 -0600

    Get ready to add more functionality in header

--- selectdefaultapplication.h
+++ selectdefaultapplication.h
@@ -28,25 +28,29 @@ private:
 	void loadIcons(const QString &path);
 	void populateApplicationList(const QString &filter);
 	void addToMimetypeList(const QString &mimetypeName, const bool selected);
-
-	// Hashtable of application names to mimetypes
-	// Used because it is difficult to extract the information from m_apps' multiple different hashtables
-	// JK its not used
-	//QMultiHash<QString,QString> m_supportedMimetypes;
-	// Hashtable with keys as parent mime types and values as all children of that mimetype which are encountered
-	QMultiHash<QString, QString> m_childMimeTypes;
+	QMultiHash<QString, QString> getDefaultDesktopEntries();
 
 	// Hashtable of application names to hashtables of mimetypes to .desktop file entries
 	QHash<QString, QHash<QString, QString> > m_apps;
 	// Hashtable of application names to icons
 	QHash<QString, QString> m_applicationIcons;
-	//QHash<QString, QString> m_applicationNames;
-	//QHash<QString, QString> m_desktopFileNames;
+	// Multi-hashtable with keys as parent mime types and values as all children of that mimetype which are encountered
+	QMultiHash<QString, QString> m_childMimeTypes;
+
+	// Set containing all the mimegroups we saw
+	QSet<QString> m_mimegroups;
+	// Multi-hashtable with keys as application names and values as mimetypes
+	QMultiHash<QString, QString> m_defaultApps;
+	// Multi-hashtable with keys as .desktop files and values as mimetypes, read from mimeapps.list
+	// Note this is opposite how they are actually stored. It is done this way so that we can read mimeapps.list before
+	// parsing anything else and then as we loop over all .desktop files, fill up the associations between programs and
+	// mimetypes. Remains constant after startup
+	QMultiHash<QString, QString> m_defaultDesktopEntries;
 
 	QHash<QString, QString> m_appIdToDesktopFile;
 
-	QHash<QString, QIcon>
-		m_mimeTypeIcons; // for preloading icons, because that's (a bit) slooow
+	// for preloading icons, because that's (a bit) slooow
+	QHash<QString, QIcon> m_mimeTypeIcons;
 	QHash<QString, QString> m_iconPaths;
 
 	QListWidget *m_applicationList;
